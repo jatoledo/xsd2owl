4cf8b65b3f3070d3338b54ba2a7c1ce2a7e881ed
Jhon Toledo Barreto <jhon.toledo.barreto@alumnos.upm.es>
2020-11-20 00:02:30 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ for file in src/test/resources/netex_reusableComponents/*
 do
     #echo $(basename $file )
     #/usr/bin/time -f
+    echo $file "***********"
     time  -f "elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file\n%e,%S,%U,%M,%K,$(basename $file )" -o tmp_time.csv java -jar target/xsd2owl-1.0.0.jar  -x $file -o output/$(basename $file ).rdf
     tail -1 tmp_time.csv >> time.csv
     # elapsed_time(seconds)
@@ -14,4 +15,4 @@ do
 
 done
 rm tmp_time.csv
-curl -G http://localhost:8086/query -u admin:admin123 --data-urlencode "q=SHOW DATABASES"
+#curl -G http://localhost:8086/query -u admin:admin123 --data-urlencode "q=SHOW DATABASES"
