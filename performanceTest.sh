99be531031c0bab08e062d288801f362187dfca0
Jhon Toledo Barreto <jhon.toledo.barreto@alumnos.upm.es>
2020-11-20 00:25:58 +0100

    Create performanceTest.sh

---
+++ performanceTest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm time.csv
+echo "elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file">>time.csv
+for file in src/test/resources/netex_reusableComponents/*
+do
+    #echo $(basename $file )
+    #/usr/bin/time -f
+    echo $file "***********"
+    time  -f "elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file\n%e,%S,%U,%M,%K,$(basename $file )" -o tmp_time.csv java -jar target/xsd2owl-1.0.0.jar  -x $file -o output/$(basename $file ).rdf
+    tail -1 tmp_time.csv >> time.csv
+    # elapsed_time(seconds)
+    # cpu_percentage[(%U + %S) / %E]
+    # kernel_mode(CPU-seconds)
+    # user_mode(CPU-seconds)
+    # memory_max(Kbytes)
+    # memory_average(Kbytes)
+
+done
+rm tmp_time.csv
+#curl -G http://localhost:8086/query -u admin:admin123 --data-urlencode "q=SHOW DATABASES"
