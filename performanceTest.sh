6b8a4076ec6e752d3d74b094da781ee0f7a986ac
Jhon Toledo Barreto <jhon.toledo.barreto@alumnos.upm.es>
2020-11-23 12:44:38 +0100

    Update performanceTest.sh

--- performanceTest.sh
+++ performanceTest.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 rm time.csv
-echo "elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file">>time.csv
+echo "run,elapsed_time,kernel_mode,User_mode,memory_max,memory_average,file">>time.csv
 for file in src/test/resources/netex_reusableComponents/*
 do
     #echo $(basename $file )
     #/usr/bin/time -f
     echo "***** $(basename $file ) *****"
-    time  -f "elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file\n%e,%S,%U,%M,%K,$(basename $file )" -o tmp_time.csv java -jar target/xsd2owl-1.0.0.jar  -x $file -o output/$(basename $file ).rdf
+    for i in 1 2 3 4 5
+    do
+    time  -f "run,elapsed_time,kernel_mode,user_mode,memory_max,memory_average,file\n,$i,%e,%S,%U,%M,%K,$(basename $file )" -o tmp_time.csv java -jar target/xsd2owl-1.0.0.jar  -x $file -o output/$(basename $file ).rdf
     tail -1 tmp_time.csv >> time.csv
+    done
     # elapsed_time(seconds)
     # cpu_percentage[(%U + %S) / %E]
     # kernel_mode(CPU-seconds)
